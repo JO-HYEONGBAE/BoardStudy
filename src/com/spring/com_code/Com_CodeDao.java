a5e3b0823f61965c0eda7c8a069ff28bca53ec95
조형배 <pwjhb@naver.com>
2020-05-19 22:59:11 +0900

    Create Com_CodeDao.java

---
+++ src/com/spring/com_code/Com_CodeDao.java
@@ -0,0 +1,11 @@
+package com.spring.com_code.dao;
+
+import java.util.List;
+
+import com.spring.com_code.vo.Com_CodeVo;
+
+public interface Com_CodeDao {
+	
+	public List<Com_CodeVo> selectType();
+
+}
