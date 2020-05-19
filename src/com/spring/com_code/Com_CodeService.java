83b03c588f69c8362333df076844417428883c81
조형배 <pwjhb@naver.com>
2020-05-19 22:58:33 +0900

    Create Com_CodeService.java

---
+++ src/com/spring/com_code/Com_CodeService.java
@@ -0,0 +1,10 @@
+package com.spring.com_code.service;
+
+import java.util.List;
+
+import com.spring.com_code.vo.Com_CodeVo;
+
+public interface com_CodeService {
+
+	public List<Com_CodeVo> selectType();
+}
